8b28142c4734b736a7e3afa33b5a52b82110d815
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-19 22:20:06 -0400

    work first move attack png was 512 by 128 512 / 4 = 128

---
+++ script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+
+// let playerImage = document.getElementById('player');
+const playerImage = new Image();
+playerImage.src = "./assets/Attack_1.png";
+// 512 / 4 = 128
+const playerWidth = 128;
+const playerHeight = 128;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0; 
+const staggerFrames = 8; 
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position = Math.floor(gameFrame/staggerFrames) % 3;
+  // context.drawImage(img, sx, sy, swidth, sheight, x, y, width, height)
+  ctx.drawImage(playerImage,playerWidth * position , playerHeight * frameY, playerWidth , playerHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+  gameFrame++
+  requestAnimationFrame(animate)
+}
+animate()
