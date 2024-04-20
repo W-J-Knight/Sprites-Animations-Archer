226e9dfbc77b810ba3b7c15535342e0efe08e7c0
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-20 05:56:29 -0400

    got other indle.png to work

--- script.js
+++ script.js
@@ -18,26 +18,31 @@ const playerImage = new Image();
 
 // move: hurt
 // 384 / 3 = 128
-playerImage.src = "./assets/Hurt.png";
+// playerImage.src = "./assets/Hurt.png";
 
 // move: dying
 // 384 / 3 = 128
-playerImage.src = "./assets/Dead.png";
+// playerImage.src = "./assets/Dead.png";
+
+// move: other idle
+// 384 / 3 = 128
+playerImage.src = "./assets/Idle.png";
 
 const playerWidth = 128;
 const playerHeight = 128;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0; 
-const staggerFrames = 40; 
+const staggerFrames = 5; 
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame/staggerFrames) % 3;
+  let position = Math.floor(gameFrame/staggerFrames) % 6;
   // context.drawImage(img, sx, sy, swidth, sheight, x, y, width, height)
   ctx.drawImage(playerImage,playerWidth * position , playerHeight * frameY, playerWidth , playerHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
   gameFrame++
+  console.log(position)
   requestAnimationFrame(animate)
 }
 animate()
