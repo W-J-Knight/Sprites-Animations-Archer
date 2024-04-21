bd5f83d0538c2b7a917b455b28df590b2146d89c
William Knight <William.Jeffery.Knight@protonmail.com>
2024-04-20 20:59:10 -0400

    finish sprit animations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.addEventListener('load', function(){
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
@@ -5,64 +6,66 @@ const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
 
-// let playerImage = document.getElementById('player');
-const playerImage = new Image();
+class Player {
+  constructor(width, height, movesObject, defaultMove) {
+    this.width = width;
+    this.height = height;
+    this.movesObject = movesObject
+    this.image = new Image()
+    this.frameX = 0;
+    this.frameY = 0;
+    this.defaultMove = defaultMove
+  }
+  update() {
+    this.move = this.movesObject[this.defaultMove]
+    this.src = this.move.src;
+    this.maxFrames = this.move.maxFrames;
+    this.movesStaggerFrames = this.move.movesStaggerFrames
+    
+  }
+  draw(position) {
+    this.image.src = this.src
+    // context.drawImage(img, sx, sy, swidth, sheight, x, y, width, height)
+    ctx.drawImage(this.image, this.width * position, this.height * this.frameY, this.width, this.height, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  }
+}
 
-// move: attack
-// 512 / 4 = 128
-// playerImage.src = "./assets/Attack_1.png";
+let gameFrame = 0;
 
-// move: idle
-// 512 / 4 = 128
-// playerImage.src = "./assets/Idle_2.png";
 
-// move: hurt
-// 384 / 3 = 128
-// playerImage.src = "./assets/Hurt.png";
+const movesOjectPlayer_1 = {
+  "idle": { src: "./assets/archer/Idle.png", maxFrames: 6, movesStaggerFrames: 8 },
+  "waiting": { src: "./assets/archer/Idle_2.png", maxFrames: 4, movesStaggerFrames: 8 },
+  "walk": { src: "./assets/archer/Walk.png", maxFrames: 8, movesStaggerFrames: 12 },
+  "run": { src: "./assets/archer/Run.png", maxFrames: 8, movesStaggerFrames: 4 },
+  "jump": { src: "./assets/archer/Jump.png", maxFrames: 9, movesStaggerFrames: 5 },
+  "hurt": { src: "./assets/archer/Hurt.png", maxFrames: 3, movesStaggerFrames: 5 },
+  "attack": { src: "./assets/archer/Attack_1.png", maxFrames: 4, movesStaggerFrames: 5 },
+  "verticalShot": { src: "./assets/archer/Shot_1.png", maxFrames: 14, movesStaggerFrames: 15 },
+  "horizonalShot": { src: "./assets/archer/Shot_2.png", maxFrames: 13, movesStaggerFrames: 15 },
+  "dying": { src: "./assets/archer/Dead.png", maxFrames: 3, movesStaggerFrames: 18 },
+  
+}
+const player_1 = new Player(128, 128, movesOjectPlayer_1, "run")
 
-// move: dying
-// 384 / 3 = 128
-// playerImage.src = "./assets/Dead.png";
+player_1.defaultMove = "waiting"
+const dropdown = document.getElementById('animations')
+dropdown.addEventListener('change', function(e){
+  player_1.defaultMove  = e.target.value;
+})
 
-// move: other idle
-// 768 / 6 = 128
-// playerImage.src = "./assets/Idle.png";
 
-// move: other jump
-// 1152 / 9 = 128
-// playerImage.src = "./assets/Jump.png";
-
-// move: other run
-// 1064 / 8 = 128
-// playerImage.src = "./assets/Run.png";
-
-// move: other shot-1
-// 1792 / 14 = 128
-// playerImage.src = "./assets/Shot_1.png";
-
-// move: other shot-2
-// 1664 / 13 = 128
-// playerImage.src = "./assets/Shot_2.png";
-
-// move: other shot-2
-// 1024 / 8 = 128
-playerImage.src = "./assets/Walk.png";
-
-const playerWidth = 128;
-const playerHeight = 128;
-let frameX = 0;
-let frameY = 0;
-let gameFrame = 0; 
-const staggerFrames = 7; 
 
 function animate() {
+  player_1.update()
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame/staggerFrames) % 8;
+  let position = Math.floor(gameFrame / player_1.movesStaggerFrames) % player_1.maxFrames;
   // context.drawImage(img, sx, sy, swidth, sheight, x, y, width, height)
-  ctx.drawImage(playerImage,playerWidth * position , playerHeight * frameY, playerWidth , playerHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-
+  player_1.draw(position)
   gameFrame++
-  console.log(position)
   requestAnimationFrame(animate)
 }
+
+
 animate()
+})
